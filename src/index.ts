6b58340ecd021a9289258af31ac3dd278d3b532e
Bilal Amir <bilalamir916@gmail.com>
2026-08-19 09:49:43 -0700

    document embedding

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Groq } from "@llamaindex/groq";
 import { HuggingFaceEmbedding } from "@llamaindex/huggingface";
 import { SimpleDirectoryReader } from "@llamaindex/readers/directory";
+import { Settings, VectorStoreIndex } from "llamaindex";
 
 const llm = new Groq({
   model: "llama-3.3-70b-versatile",
@@ -10,6 +11,8 @@ const embedModel = new HuggingFaceEmbedding({
   modelType: "BAAI/bge-small-en-v1.5",
 });
 
+Settings.embedModel = embedModel
+
 console.log("LLM and embedding model configured.");
 
 const reader = new SimpleDirectoryReader();
@@ -18,4 +21,6 @@ const documents = await reader.loadData({
   directoryPath: "data",
 });
 
-console.log(documents);
+const index = await VectorStoreIndex.fromDocuments(documents); 
+
+console.log("Index created.", index);
